7746ca3c47a24d09d4ff92dc8476a7bca9a7a1bc
SaneMyburg <sane.myburg@gmail.com>
2023-03-29 18:06:49 +0200

    Install React Router DOM

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const Layout = () => (
+  <div>
+    <Navbar />
+    <Outlet />
+  </div>
+);
+
+export default Layout;
